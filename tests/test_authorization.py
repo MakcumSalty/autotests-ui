039490f3c3fec3de056000783592ce56001fa417
MakcumSalty <maksisaltey1991@gmail.com>
2026-04-22 17:06:12 +0300

    Fixtures Pytest

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,40 +1,34 @@
-from playwright.sync_api import sync_playwright, expect  # Импорт Playwright для синхронного режима и проверки
+from playwright.sync_api import Playwright, Page, expect  # Импорт Playwright для синхронного режима и проверки
 import pytest
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_data_authorization():
-    # Запуск Playwright в синхронном режиме
-    with sync_playwright() as playwright:
-        # Открываем браузер Chromium (не в headless режиме, чтобы видеть действия)
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()  # Создаем новую страницу
+def test_wrong_data_authorization(chromium_page: Page):
+    # Переходим на страницу авторизации
+    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-        # Переходим на страницу авторизации
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    # Находим поле "Email" и заполняем его
+    # email_input = page.locator('//div[@data-testid="login-form-email-input"]//div//input')
+    email_input = chromium_page.get_by_test_id("login-form-email-input").locator('input')
+    email_input.fill("user.name@gmail.com")
 
-        # Находим поле "Email" и заполняем его
-        # email_input = page.locator('//div[@data-testid="login-form-email-input"]//div//input')
-        email_input = page.get_by_test_id("login-form-email-input").locator('input')
-        email_input.fill("user.name@gmail.com")
+    # page.wait_for_timeout(2000)
 
-        # page.wait_for_timeout(2000)
+    # Находим поле "Password" и заполняем его
+    # password_input = page.locator('//div[@data-testid="login-form-password-input"]//div//input')
+    password_input = chromium_page.get_by_test_id("login-form-password-input").locator('input')
+    password_input.fill("password")
 
-        # Находим поле "Password" и заполняем его
-        # password_input = page.locator('//div[@data-testid="login-form-password-input"]//div//input')
-        password_input = page.get_by_test_id("login-form-password-input").locator('input')
-        password_input.fill("password")
+    # page.wait_for_timeout(1000)
 
-        # page.wait_for_timeout(1000)
+    # Находим кнопку "Login" и кликаем на нее
+    # login_button = page.locator('//button[@data-testid="login-page-login-button"]')
+    login_button = chromium_page.get_by_test_id("login-page-login-button")
+    login_button.click()
 
-        # Находим кнопку "Login" и кликаем на нее
-        # login_button = page.locator('//button[@data-testid="login-page-login-button"]')
-        login_button = page.get_by_test_id("login-page-login-button")
-        login_button.click()
-
-        # Проверяем, что появилось сообщение об ошибке
-        # wrong_email_or_password_alert = page.locator('//div[@data-testid="login-page-wrong-email-or-password-alert"]')
-        wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
-        expect(wrong_email_or_password_alert).to_be_visible()  # Проверяем видимость элемента
-        expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")  # Проверяем текст
+    # Проверяем, что появилось сообщение об ошибке
+    # wrong_email_or_password_alert = page.locator('//div[@data-testid="login-page-wrong-email-or-password-alert"]')
+    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
+    expect(wrong_email_or_password_alert).to_be_visible()  # Проверяем видимость элемента
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")  # Проверяем текст
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,75 +1,27 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import Playwright, expect, Page
 import pytest
+
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        # Запускаем Chromium браузер в обычном режиме (не headless)
-        browser = playwright.chromium.launch(headless=False)
-        # Создаем новый контекст браузера (новая сессия, которая изолирована от других)
-        context = browser.new_context()
-        # Открываем новую страницу в рамках контекста
-        page = context.new_page()
+def test_empty_courses_list(chromium_page_with_state: Page):
+    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        # Переходим на страницу авторизации
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    # Проверка заголовка страницы
+    header = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
+    expect(header).to_be_visible()
+    expect(header).to_have_text("Courses")
 
-        # Находим поле "Email" и заполняем его
-        # email_input = page.locator('//div[@data-testid="registration-form-email-input"]//div//input')
-        email_input = page.get_by_test_id("registration-form-email-input").locator('input')
-        email_input.fill("user.name@gmail.com")
+    # Проверка иконки на странице
+    icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
+    expect(icon).to_be_visible()
 
-        # Находим поле "Username" и заполняем его
-        # username_input = page.locator('//div[ @ data - testid = "registration-form-username-input"] // div // input')
-        username_input = page.get_by_test_id("registration-form-username-input").locator('input')
-        username_input.fill("username")
+    # Проверка текста на странице
+    text_1 = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
+    expect(text_1).to_be_visible()
+    expect(text_1).to_have_text("There is no results")
 
-        # Находим поле "Password" и заполняем его
-        # password_input = page.locator('//div[@data-testid="registration-form-password-input"]//div//input')
-        password_input = page.get_by_test_id("registration-form-password-input").locator('input')
-        password_input.fill("password")
-
-        page.wait_for_timeout(2000)
-
-        # Находим кнопку "Registration" и кликаем на нее
-        # registration_button = page.locator('//button[@data-testid="registration-page-registration-button"]]')
-        registration_button = page.get_by_test_id("registration-page-registration-button")
-        registration_button.click()
-
-        page.wait_for_timeout(2000)
-
-        # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
-        context.storage_state(path="browser-state.json")
-
-    with sync_playwright() as playwright:
-        # Запускаем Chromium браузер в обычном режиме (не headless)
-        browser = playwright.chromium.launch(headless=False)
-        # Указываем файл с сохраненным состоянием
-        context = browser.new_context(storage_state="browser-state.json")
-        # Открываем новую страницу в рамках контекста
-        page = context.new_page()
-
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-        page.wait_for_timeout(2000)
-
-        # Проверка заголовка страницы
-        header = page.get_by_test_id("courses-list-toolbar-title-text")
-        expect(header).to_be_visible()
-        expect(header).to_have_text("Courses")
-
-        # Проверка иконки на странице
-        icon = page.get_by_test_id("courses-list-empty-view-icon")
-        expect(icon).to_be_visible()
-
-        # Проверка текста на странице
-        text_1 = page.get_by_test_id("courses-list-empty-view-title-text")
-        expect(text_1).to_be_visible()
-        expect(text_1).to_have_text("There is no results")
-
-        # Проверка текста на странице
-        text_2 = page.get_by_test_id("courses-list-empty-view-description-text")
-        expect(text_2).to_be_visible()
-        expect(text_2).to_have_text("Results from the load test pipeline will be displayed here")
-
+    # Проверка текста на странице
+    text_2 = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
+    expect(text_2).to_be_visible()
+    expect(text_2).to_have_text("Results from the load test pipeline will be displayed here")
