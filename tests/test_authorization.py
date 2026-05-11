ba29051e4e9741d60ad2468df66b271b9c7072fc
MakcumSalty <maksisaltey1991@gmail.com>
2026-05-11 21:59:24 +0300

    Task Parametrization Pytest

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -4,21 +4,28 @@ import pytest
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_data_authorization(chromium_page: Page):
+@pytest.mark.parametrize('email, password',
+        [
+            ("user.name@gmail.com", "password"),
+            ("user.name@gmail.com", "  "),
+            ("  ", "password")
+        ]
+)
+def test_wrong_data_authorization(chromium_page: Page, email: str, password: str):
     # Переходим на страницу авторизации
     chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
     # Находим поле "Email" и заполняем его
     # email_input = page.locator('//div[@data-testid="login-form-email-input"]//div//input')
     email_input = chromium_page.get_by_test_id("login-form-email-input").locator('input')
-    email_input.fill("user.name@gmail.com")
+    email_input.fill(email)
 
     # page.wait_for_timeout(2000)
 
     # Находим поле "Password" и заполняем его
     # password_input = page.locator('//div[@data-testid="login-form-password-input"]//div//input')
     password_input = chromium_page.get_by_test_id("login-form-password-input").locator('input')
-    password_input.fill("password")
+    password_input.fill(password)
 
     # page.wait_for_timeout(1000)
 
