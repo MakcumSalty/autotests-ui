e2ca3aebd4df4db96c12b19d5e03e691ec1ae7c0
MakcumSalty <maksisaltey1991@gmail.com>
2026-05-18 15:24:16 +0300

    Тask РageСomponent

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,9 +1,14 @@
+from components.navigation.navbar_component import NavbarComponent
+from components.navigation.sidebar_component import SidebarComponent
 from pages.base_page import BasePage
 from playwright.sync_api import Page, expect
 
 class CoursesListPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+
+        self.navbar = NavbarComponent(page)
+        self.sidebar = SidebarComponent(page)
 
         # Заголовок и кнопка создания курса
         self.courses_title = page.get_by_test_id("courses-list-toolbar-title-text")
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -39,29 +39,34 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
     )
 
 
-
-
-
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state: Page):
-    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
-    # Проверка заголовка страницы
-    header = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
-    expect(header).to_be_visible()
-    expect(header).to_have_text("Courses")
+    courses_list_page.navbar.check_visible('username')
+    courses_list_page.sidebar.check_visible()
 
-    # Проверка иконки на странице
-    icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
-    expect(icon).to_be_visible()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_empty_view()
 
-    # Проверка текста на странице
-    text_1 = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
-    expect(text_1).to_be_visible()
-    expect(text_1).to_have_text("There is no results")
 
-    # Проверка текста на странице
-    text_2 = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
-    expect(text_2).to_be_visible()
-    expect(text_2).to_have_text("Results from the load test pipeline will be displayed here")
+    # # Проверка заголовка страницы
+    # header = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
+    # expect(header).to_be_visible()
+    # expect(header).to_have_text("Courses")
+    #
+    # # Проверка иконки на странице
+    # icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
+    # expect(icon).to_be_visible()
+    #
+    # # Проверка текста на странице
+    # text_1 = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
+    # expect(text_1).to_be_visible()
+    # expect(text_1).to_have_text("There is no results")
+    #
+    # # Проверка текста на странице
+    # text_2 = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
+    # expect(text_2).to_be_visible()
+    # expect(text_2).to_have_text("Results from the load test pipeline will be displayed here")
