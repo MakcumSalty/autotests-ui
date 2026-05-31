44a67ca076a5cf281c83f4128787652b283eb9d4
MakcumSalty <maksisaltey1991@gmail.com>
2026-06-01 01:36:03 +0300

    Hастройки автотестов с Pydantic

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,12 +1,10 @@
 from typing import Any, Generator
-
-import allure
 import pytest
 from _pytest.fixtures import SubRequest
 from playwright.sync_api import Playwright, Page
 from pages.authentication.registration_page import RegistrationPage
 from tools.playwright.pages import initialize_playwright_page
-
+from config import settings
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Generator[Any, None, None]:  # Добавили аргумент request
@@ -14,17 +12,20 @@ def chromium_page(request: SubRequest, playwright: Playwright) -> Generator[Any,
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=settings.headless)
     context = browser.new_context()
     page = context.new_page()
 
     registration_page = RegistrationPage(page)
     registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.registration_form.fill(email="user.name@gmail.com", username="username", password="password")
+    registration_page.registration_form.fill(
+        email=settings.test_user.email,
+        username=settings.test_user.username,
+        password=settings.test_user.password)
     registration_page.click_registration_button()
 
     # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
-    context.storage_state(path="browser-state.json")
+    context.storage_state(path=settings.browser_state_file)
     browser.close()
 
 @pytest.fixture
@@ -33,5 +34,5 @@ def chromium_page_with_state(initialize_browser_state, request: SubRequest, play
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
-        storage_state="browser-state.json"
+        storage_state=settings.browser_state_file
     )
